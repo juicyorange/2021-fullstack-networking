402e17ed5b514bcfd494623dcd03e45cf8b622dc
juicyorange <rlaxodud9809@khu.ac.kr>
2021-11-29 22:27:22 +0900

    Fix : tcp_echo 01-02

--- tcp_echo/lec-03-prg-01-tcp-echo-server.js
+++ tcp_echo/lec-03-prg-01-tcp-echo-server.js
@@ -5,8 +5,10 @@ const host = "127.0.0.1";
 const port = 10397;
 
 // 소켓 서버를 생성한다.
+// createServer의 파라미터로 연결이 들어왔을때 실행할 콜백함수를 넣어준다.
 const server = net.createServer((socket) => {
   socket.setEncoding("utf8"); // utf8을 이용하여 인코딩하겠다고 명시.
+
   console.log(
     "> client connected by Ip address " +
       socket.address().address +
@@ -15,35 +17,23 @@ const server = net.createServer((socket) => {
   );
 
   let RecvData;
-  // client로 부터 오는 data를 화면에 출력
+
+  // 소켓으로부터 데이터가 들어온 것을 감지하는 event listner
+  // 이벤트가 들어오면 지정한 call back 함수가 호출된다.
   socket.on("data", (data) => {
     console.log("> echoed: " + data);
     RecvData = data;
     if (RecvData === "quit") {
       console.log("> echo-server is de-activated");
-      socket.destroy();
       exit();
     }
     // 클라이언트에게 소켓을 통해 메시지를 전달한다.
-    writeData(socket, data);
+    socket.write(data);
   });
 });
 
-// 서버를 동작시킨다.
-// 서버가 돌아갈 host, port 넘버를 지정해준다.
+// 서버가 특정 host, port에 대해 요청이 들어오는 것을 확인하도록 listen하도록 한다.
+// 서버가 요청이 오기를 기다리는 listen 상태가 된다.
 server.listen(port, host, () => {
   console.log("> echo-server is activated");
 });
-
-// 데이터 보내기
-// 특정 socket에게 메시지를 전달한다.
-function writeData(socket, data) {
-  var success = socket.write(data);
-  if (success) {
-    (function (socket, data) {
-      socket.once("drain", function () {
-        writeData(socket, data);
-      });
-    })(socket, data);
-  }
-}
--- tcp_echo/lec-03-prg-02-tcp-echo-client.js.js
+++ tcp_echo/lec-03-prg-02-tcp-echo-client.js.js
@@ -10,57 +10,41 @@ const r = readline.createInterface({
   output: process.stdout,
 });
 
-let input;
-
-r.setPrompt("> ");
-r.on("line", (line) => {
-  input = line;
-});
-
 // 소켓 연결
 const socket = net.connect({ host, port });
 socket.setEncoding("utf8");
 
-// 소켓 연결되었을때
-socket.on("connect", function () {
+// 소켓이 성공적으로 연결된 것을 감지하는 event listner
+// 이벤트가 들어오면 지정한 call back 함수가 호출된다.
+socket.on("connect", () => {
   console.log("> echo-client is activated");
-  // 소켓이 연결되면 바로 입력을 받도록한다.
 
+  // 키보드 입력을 받는다.
   r.question("> ", (input) => {
+    // 키보드 입력이 완료되면 socket.write를 통해 서버로 데이터를 보낸다.
     socket.write(input);
+    // quit 명령어가 들어오면 종료한다.
     if (input === "quit") {
       console.log("> echo-client is de-activated");
-      socket.destroy();
 
       r.close();
     }
   });
-  // quit 명령어가 들어오면 종료한다.
 });
 
-// 서버에서 데이터 받기
+// 소켓으로부터 데이터가 들어온 것을 감지하는 event listner
+// 이벤트가 들어오면 지정한 call back 함수가 호출된다.
 socket.on("data", (data) => {
   console.log("> received:", data);
 
   r.question("> ", (input) => {
+    // socket.write를 통해 서버에 데이터를 보낸다.
     socket.write(input);
+    // quit 명령어가 들어오면 종료한다.
     if (input === "quit") {
       console.log("> echo-client is de-activated");
-      socket.destroy();
 
       r.close();
     }
   });
 });
-
-// 서버에 데이터 보내기
-function writeData(socket, data) {
-  var success = socket.write(data);
-  if (success) {
-    (function (socket, data) {
-      socket.once("drain", function () {
-        writeData(socket, data);
-      });
-    })(socket, data);
-  }
-}
