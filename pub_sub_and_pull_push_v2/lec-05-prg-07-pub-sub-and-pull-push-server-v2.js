f415b82c2efe5c3fc057962546e6867b3a1a4650
juicyorange <rlaxodud9809@khu.ac.kr>
2021-12-01 15:40:37 +0900

    Add : pub-sub and pull-push v2

---
+++ pub_sub_and_pull_push_v2/lec-05-prg-07-pub-sub-and-pull-push-server-v2.js
@@ -0,0 +1,24 @@
+/* -------- pub-sub and pull-push 패턴 -----------*/
+
+const zmq = require("zeromq");
+
+// pub를 수행하기 위한 소켓을 만든다.
+const publisher = zmq.socket("pub");
+
+// 정보를 당겨오는 pull을 수행할 소켓을 생성한다.
+const collector = zmq.socket("pull");
+
+// publisher를 tcl://*:5556 에 바인드 시켜준다.
+// 지정한 address 에 소켓을 매핑했다고 생각하면된다.
+publisher.bindSync("tcp://*:5557");
+
+// collector를 tcl://*:5558 에 바인드 시켜준다.
+// 지정한 address 에 소켓을 매핑했다고 생각하면된다.
+collector.bindSync("tcp://*:5558");
+
+// pull 에 메시지가 도착한 것을 감지하는 event listener
+// 메시지가 도착했으면 콜백함수를 수행한다.
+collector.on("message", (data) => {
+  console.log("server: publishing update =>", data.toString());
+  publisher.send(data.toString());
+});
---
+++ pub_sub_and_pull_push_v2/lec-05-prg-08-pub-sub-and-pull-push-client-v2.js
@@ -0,0 +1,65 @@
+/* -------- pub-sub and pull-push 패턴 -----------*/
+
+const zmq = require("zeromq");
+
+// sub 를 수행하기 위한 소켓을 만든다.
+const subscriber = zmq.socket("sub");
+// push 를 수행하여 정보를 publish 할 소켓을 만든다.
+const publisher = zmq.socket("push");
+
+// subscribe를 수행한다. 이때 topic은 모든 topic을 받기로 한다.
+subscriber.subscribe("");
+
+// node에서는 event listener가 비동기적으로 이벤트를 확인하고 있고,
+// 해당 이벤트가 발생하면 각각의 콜백함수가 비동기적으로 수행된다.
+
+// 따라서 node는 기본적으로 비동기적으로 수행되기 때문에 poll을 통해서 특정 소켓에 이벤트가 있는지 확인하고 처리하는 것을 관리해줄 필요가 없다.
+
+// 즉, 파이썬의 예제에서는 subscriber에 날라온 데이터가 있는경우
+// poll을 통해 식별하고, 메시지를 출력하는 것이 수행되고, 그렇지 않다면 랜덤숫자를 보내는 부분이 수행되는데
+// node는 비동기적으로 이벤트를 listen하고 콜백함수를 수행하기 떄문에 그럴 필요가 없다.
+
+// 가독성을 위해 프로그램을 실행할때 클라이언트 이름을 받는다.
+clientID = process.argv[2];
+
+// sub 에 메시지가 도착한 것을 감지하는 event listener
+// 메시지가 도착했으면 콜백함수를 수행한다.
+subscriber.on("message", (data) => {
+  console.log(clientID, ": receive status =>", data.toString());
+});
+
+const publishMessage = () => {
+  let randNum = Math.floor(Math.random() * 101);
+
+  // 1초 뒤에 안의 콜백함수가 수행된다.
+
+  if (randNum < 10) {
+    let msg = "(" + clientID + " :ON)";
+    console.log(clientID, ": send status - activated");
+
+    // push를 통해 pull 하는 곳으로 메시지를 보낸다.
+    publisher.send(msg);
+
+    setTimeout(() => {
+      publishMessage();
+    }, 1000);
+  } else if (randNum > 90) {
+    let msg = "(" + clientID + " :OFF)";
+    console.log(clientID, ": send status - deactivated");
+
+    // push를 통해 pull 하는 곳으로 메시지를 보낸다.
+    publisher.send(msg);
+    setTimeout(() => {
+      publishMessage();
+    }, 1000);
+  } else {
+    publishMessage();
+  }
+};
+
+publishMessage();
+
+// publisher가 동작하고 있는 address에 연결한다.
+subscriber.connect("tcp://localhost:5557");
+// collector가 동작하고 있는 address에 연결한다.
+publisher.connect("tcp://localhost:5558");
